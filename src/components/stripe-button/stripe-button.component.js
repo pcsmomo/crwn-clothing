913a45d1761ac7c047d59fa0c7b9dcc4c7787b0c
pcsmomo <dongwoo_333@hanmail.net>
2021-04-17 15:53:23 +1000

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,31 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_qX5l3nkBerpHx6C08VrGriCo";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      ammount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+      locale="en"
+      currency="AUD"
+    />
+  );
+};
+
+export default StripeCheckoutButton;
